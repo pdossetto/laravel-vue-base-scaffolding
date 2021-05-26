806677f72af916aad669809351efabc2be889fc2
Paolo Dossetto <dossettopaolo@gmail.com>
2021-05-26 07:25:45 +0200

    Base Scaffolding Laravel-Vue per nuovi progetti

---
+++ resources/js/pages/Home.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <section class="mt-5">
+            <div class="container">
+                <div class="row">
+                    <div class="col 12">
+                        <h1 class="text-center font-weight-bold">
+                            {{ message }}
+                        </h1>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                message: 'Hello World!'
+            }
+        }
+    }
+</script>
---
+++ resources/js/views/App.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <header>
+            <div class="container">
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                    <a class="navbar-brand" href="#" id="logo">
+                        <img :src="logoImg">
+                    </a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav mr-auto">
+                            <li class="nav-item active">
+                                <router-link class="nav-link" :to="{ name: 'home' }">
+                                    Home
+                                </router-link>
+                            </li>
+                            <li class="nav-item active">
+                                <router-link class="nav-link" :to="{ name: 'contatti' }">
+                                    Contatti
+                                </router-link>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </header>
+        <main>
+            <router-view />
+        </main>
+    </div>
+</template>
+<script>
+    export default {
+        name: "App",
+        data() {
+            return {
+                logoImg: "https://www.boolean.careers/images/misc/logo.png"
+            }
+        }
+    }
+</script>
+<style scoped>
+    #logo img {
+        height: 30px;
+    }
+</style>
